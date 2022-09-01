dc5fe5d77a2e574c7661a0b4c8092279e15a0286
Tim Menzies <timm@ieee.org>
2022-09-01 14:13:39 -0400

    coerce

--- src/tmp.jl
+++ src/tmp.jl
@@ -1,20 +1,4 @@
 # vim: ts=2 sw=2 et : 
-help="asa
-asd
-asd
-as
-asdas"
-
-phone = r"\((\d{3})\)-(\d{3})-(\d{4})"
-text = """
-       My phone number is (555)-505-1000.
-       Her phone number is (555)-999-9999.
-       """
-
-print(ARGS)
-
-for m in eachmatch(phone, text)
-           println("Matched $(m.match) with area code $(m[1])") end
 
 function coerce(s)
   if     ((x=tryparse(Int64,s))   != nothing)  x 
@@ -30,25 +14,27 @@ function cli(d::Dict)
       if v == "-" * slot[1] 
         d[slot] = coerce(x==true  ? "false" : (
                          x==false ? "true"  : (
-                         ARGS[i+1]))) end end end
-  d end 
+                         ARGS[i+1]))) end end end; d end 
 
-d =Dict()
+function settings(s)
+  d=Dict()
+  rWant = r"\n[\s]+[-][^-]+[-][-]([\S]+)[^=]+=[\s]+([\S]+)"
+  for m in eachmatch(rWant, s) d[m[1]] = coerce(m[2]) end
+  d end
 
-text = """
-asda asd
+help = """
+       asda asd
+ 
+       Fred: sdsaas
+       OPTIONS
+ 
+        -a --aaaa asdas  = 10.1
+        -b --bbbb asdas  = 20 
+        -n --norm asdas  = true
+       """
 
-Fred: sdsaas
-OPTIONS
+println(cli((settings(help))))
 
- -a --aaaa asdas  = 10.1
- -b --bbbb asdas  = 20 
- -n --norm asdas  = true
-"""
-want = r"\n [-][^-]+[-][-]([\S]+)[^=]+=[\s]+([\S]+)"
-for m in eachmatch(want, text) d[m[1]] = coerce(m[2]) end
-println(d)
-println(cli(d))
 # the=[]
 # setting(words) = (the[words[1][2:end]] = coerce(words[end]))
 #
