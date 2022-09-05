81c7420a7d671f724e518aec539744a0d1609225
Tim Menzies <timm@users.noreply.github.com>
2022-09-05 12:32:50 -0400

    save

--- src/lib/2thing.jl
+++ src/lib/2thing.jl
@@ -3,8 +3,9 @@ function coerce(s)
   for t in [Int64,Float64,Bool] if (x=tryparse(t,s)) != nothing return x end end 
   return strip(s) end
 
-"Coerce csv rows to cells."
-function csv(file, fun)
+"Coerce rows to cells. Pass each row to `fun`."
+function rows(src::Array, fun) for one in src fun(one) end
+function rows(src::String, fun)
   for line in eachline(file)
     line = strip(line)
     if sizeof(line) > 0 fun(map(coerce, split(line, ","))) end end end 
